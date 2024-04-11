434946d822d24dba60337cf12e1384af20ab328e
Jignesh Goyal <jigneshgoyal114@gmail.com>
2024-04-11 16:17:46 +0530

    update upload page

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -4,35 +4,10 @@ import "../App.css";
 const Uplaod = () => {
   return (
     <div className="relative min-h-screen">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-        autoPlay
-        loop
-        muted
-      >
-        <source
-          src="https://res.cloudinary.com/dtonbpwvj/video/upload/v1712767204/music-video_hmmcpb.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <img src="https://wallpapercave.com/wp/wp7033570.jpg" className="backdrop-blur-xl h-screen w-screen object-cover absolute z-0 " alt="" />
 
       <div className="relative z-10 bg-black bg-opacity-75 min-h-screen flex flex-col justify-center items-center">
-        <div className="flex justify-between w-full px-4">
-          <div className="flex items-center">
-            <div className="w-5 h-5 bg-red-500 rounded-full mr-4"></div>
-            <h1 className="text-white text-2xl font-bold">MY UPLOADS</h1>
-          </div>
-          <div className="flex">
-            <a
-              href="#"
-              className="bg-black hover:bg-red-500 border-2 border-red-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300"
-            >
-              Home
-            </a>
-          </div>
-        </div>
-
-        <div className="bg-black rounded-lg p-8 mt-8 flex flex-col md:flex-row shadow-lg">
+        <div className="bg-gradient-to-r from-orange-200 to-fuchsia-400 rounded-lg p-8 mt-8 flex flex-col md:flex-row shadow-lg">
           <div className="mb-8 md:mr-20 md:mb-0">
             <div className="mb-4">
               <img
@@ -41,13 +16,13 @@ const Uplaod = () => {
                 className="w-full h-auto rounded-lg border border-gray-500"
               />
             </div>
-            <div className="text-white font-bold text-lg relative mb-4">
-              Upload Music
-              <div className="h-1 bg-red-500 absolute bottom-0 left-0 right-0"></div>
-            </div>
-            <div className="text-white font-bold text-lg relative mb-4">
-              Upload Cover
-              <div className="h-1 bg-red-500 absolute bottom-0 left-0 right-0"></div>
+            <div className="flex flex-col gap-2">
+            <button className="border bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...  rounded-full text-white p-2">
+         Upload music
+            </button>
+            <button className="border bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... bg-red-600 rounded-full text-white p-2">
+            Upload cover
+            </button>
             </div>
           </div>
           <div className="flex-1">
@@ -93,28 +68,27 @@ const Uplaod = () => {
                   placeholder="Proposed Release Date"
                 />
               </div>
-              <div className="mb-4">
-                <label className="text-white block mb-1">
-                  Music Collecting Society (e.g. SUISA)
-                </label>
-                <div className="flex items-center">
+              <div>
+              <div className="">
                   <input
                     type="checkbox"
                     id="yes"
                     className="mr-2 text-red-500 focus:ring-red-500"
                   />
-                  <label className="text-white mr-4" htmlFor="yes">
-                    Yes
+                  <label className="text-white mr-4" htmlFor="">
+                    Upload cover by Leeloop
                   </label>
+                  </div>
+                  <div className="mb-3">
                   <input
                     type="checkbox"
                     id="no"
-                    className="text-red-500 focus:ring-red-500"
+                    className="mr-2 text-red-500 focus:ring-red-500"
                   />
-                  <label className="text-white" htmlFor="no">
-                    No
+                  <label className="text-white" htmlFor="">
+                    Music Mastering
                   </label>
-                </div>
+                  </div>
               </div>
               <button className="bg-black border-2 border-red-500 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
                 <svg
